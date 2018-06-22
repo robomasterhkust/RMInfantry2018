18110f9ce75b148ca207a27a85fb8582bbc36b18
zengkuang <kzeng@connect.ust.hk>
2018-06-22 12:50:09 +0800

    Heatlimit change to roboconf.h

--- 0_Gimbal/dev/roboconf.h
+++ 0_Gimbal/dev/roboconf.h
@@ -9,9 +9,9 @@
   //#define RM_CHASSIS_STANDARD
   #define CHASSIS_POWER_MAX_W        80U
   #define CHASSIS_POWER_BUFFER_J     60U
-  #define LEVEL1_HEATLIMIT    90
-  #define LEVEL2_HEATLIMIT    180
-  #define LEVEL3_HEATLIMIT    360
+  #define LEVEL1_HEATLIMIT    90U
+  #define LEVEL2_HEATLIMIT    180U
+  #define LEVEL3_HEATLIMIT    360U
 #endif //RM_INFANTRY
 
 #ifdef RM_HERO
